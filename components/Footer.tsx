ab178fd296b7fba09185bb1e3e78efeed3febb80
ThynkNeeraj <thynk.neeraj@gmail.com>
2024-12-27 15:35:44 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,28 +8,25 @@ function Footer() {
     <footer className="footer flex flex-col bg-[#0D2000] text-white border-t-2 border-gray-300 gap-0">
       <div className="container mx-auto flex flex-col md:flex-row gap-0 justify-between p-6 md:p-10">
         {/* First Column - Logo, Content, and Social Icons */}
+
         <div className="w-full md:w-1/4 mb-8 text-left pl-1 md:pl-8">
-          <Image
-            className="z-0 mb-4 md:mx-0"
-            src={signature}
-            width={200}
-            height={87}
-            alt="Logo"
-          />
+          <Link href="/" passHref><Image className="z-0 mb-4 md:mx-0" src={signature} width={200} height={87} alt="Logo"></Image></Link>
+
           <p className="text-sm mt-2 mb-4">Established in 2015, Eaze Tours is driven by three travel professionals, with decades of combined varied experience in the tour industry.</p>
-          <div className="flex justify-start md:justify-start space-x-4 my-4">
+          <div className="flex justify-start md:justify-start space-x-2 my-4">
+
             <a href="https://www.tripadvisor.in/Attraction_Review-g304551-d17734269-Reviews-EAZE_TOURS-New_Delhi_National_Capital_Territory_of_Delhi.html" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-600 transition-colors">
-              <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300"><i className="fab fa-tripadvisor text-lg leading-lg" /></span>
-            </a>
-            <a href="https://www.pinterest.com/eazetourpackages/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-600 transition-colors">
-              <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300"><i className="fab fa-pinterest text-sm leading-lg" /></span>
-            </a>
+              <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300"><i className="fab fa-tripadvisor text-lg leading-lg" /></span></a>
+
             <a href="https://www.facebook.com/eazetour/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-600 transition-colors">
-              <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300"><i className="fab fa-facebook-f text-lg leading-lg" /></span>
-            </a>
+              <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300"><i className="fab fa-facebook-f text-lg leading-lg" /></span></a>
+
+            <a href="https://www.pinterest.com/eazetourpackages/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-600 transition-colors">
+              <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300"><i className="fab fa-pinterest text-sm leading-lg" /></span></a>
+
             <a href="https://www.instagram.com/eazetourpackages/" target="_blank" rel="noopener noreferrer" className="text-white hover:text-blue-600 transition-colors">
-              <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300"><i className="fab fa-instagram text-lg leading-lg" /></span>
-            </a>
+              <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300"><i className="fab fa-instagram text-lg leading-lg" /></span></a>
+
           </div>
         </div>
 
@@ -49,11 +46,11 @@ function Footer() {
         <div className="w-full md:w-1/4 mb-8 pl-1 md:pl-[80px] md:pt-[40px]">
           <h5 className="text-lg font-semibold mb-4">Top Destinations</h5>
           <ul className="space-y-4">
-            <li><Link href="/packages" className="hover:text-blue-600 transition-colors">Delhi</Link></li>
-            <li><Link href="/packages" className="hover:text-blue-600 transition-colors">Jaipur</Link></li>
-            <li><Link href="/packages" className="hover:text-blue-600 transition-colors">Agra</Link></li>
-            <li><Link href="/packages" className="hover:text-blue-600 transition-colors">Varanasi</Link></li>
-            <li><Link href="/packages" className="hover:text-blue-600 transition-colors">Goa</Link></li>
+            <li><Link href="/packages/28" className="hover:text-blue-600 transition-colors">Delhi</Link></li>
+            <li><Link href="/packages/34" className="hover:text-blue-600 transition-colors">Jaipur</Link></li>
+            <li><Link href="/packages/35" className="hover:text-blue-600 transition-colors">Agra</Link></li>
+            <li><Link href="/packages/21" className="hover:text-blue-600 transition-colors">Varanasi</Link></li>
+            <li><Link href="/packages/16" className="hover:text-blue-600 transition-colors">Goa</Link></li>
           </ul>
         </div>
 
@@ -78,13 +75,14 @@ function Footer() {
               <i className="fa fa-map-marker-alt text-lg text-white-400 -mt-1" />
               <p className="text-sm">Eaze House ~2nd Floor, RZP-146 Palam Colony, New Delhi, South West Delhi, 110075</p>
             </div>
+
           </div>
         </div>
       </div>
 
       {/* Copyright Section */}
       <div className="w-full bg-[#0D2000] text-white py-7 text-center justify-center border-t border-white border-opacity-50">
-        <p className="text-sm">&copy; Copyright eazetours.com  2024. All right reserved.</p>
+        <p className="text-sm">&copy; Copyright <a href="/" className="hover:text-blue-600">Eaze Tours</a> 2024. All Rights Reserved.</p>
       </div>
     </footer>
   );
