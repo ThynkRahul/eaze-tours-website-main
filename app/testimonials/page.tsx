efa6c2f5c5fb101089804ab93d9286d325f10e27
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-01-07 16:09:16 +0530

    Update page.tsx

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -9,9 +9,9 @@ import Testimonial from "../../components/Testimonial";
 export default function Testimonials() {
     const testimonials = testimonialData
     return (
-      <div>
-          <div className="hero min-h-screen mt-[135px]">
-                <Image 
+        <div>
+            <div className="hero min-h-screen">
+                <Image
                     className="z-0"
                     src={backgroundImage}
                     fill
@@ -20,24 +20,24 @@ export default function Testimonials() {
                         objectPosition: "66% 50%",
                     }}
                     alt="BackgroundImage"
-                    >
+                >
                 </Image>
                 <div className="hero-content absolute left-0">
                     <div className="text-left text-5xl mt-40 text-secondary">
                         <div className='backdrop-opacity-25 backdrop-blur-sm bg-white/30 p-4 rounded-full'>
-                            <h1 className="mr-10">We let our customers<br/>talk for us.</h1>
+                            <h1 className="mr-10">We let our customers<br />talk for us.</h1>
                         </div>
-                        
+
                     </div>
                 </div>
-          </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 m-1 place-items-center">
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-1 place-items-center my-12 mx-8">
                 {testimonials.map(testimonial => (
-                    <div key={testimonial.Id}>
+                    <div key={testimonial.Id} className="h-[100%] mb-8">
                         <Testimonial testimonial={testimonial}></Testimonial>
                     </div>
                 ))}
             </div>
-      </div>
+        </div>
     )
-  }
+}
