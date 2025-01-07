6c7b4ae3f31a4ed55ef1461766d593d320b2adf6
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-01-07 17:57:21 +0530

    Update page.tsx

--- app/testimonials/page.tsx
+++ app/testimonials/page.tsx
@@ -1,36 +1,26 @@
 'use client';
 
-import Image from 'next/image'
-import backgroundImage from '../../public/images/gallery/5.jpg'
 import testimonialData from '../../data/testimonials.json'
 import Testimonial from "../../components/Testimonial";
-
 
 export default function Testimonials() {
     const testimonials = testimonialData
     return (
         <div>
-            <div className="hero min-h-screen">
-                <Image
-                    className="z-0"
-                    src={backgroundImage}
-                    fill
-                    style={{
-                        objectFit: "cover",
-                        objectPosition: "66% 50%",
-                    }}
-                    alt="BackgroundImage"
-                >
-                </Image>
-                <div className="hero-content absolute left-0">
-                    <div className="text-left text-5xl mt-40 text-secondary">
-                        <div className='backdrop-opacity-25 backdrop-blur-sm bg-white/30 p-4 rounded-full'>
-                            <h1 className="mr-10">We let our customers<br />talk for us.</h1>
-                        </div>
+            <div className="relative mt-[150px] max-w-screen-xl mx-8 h-[480px] rounded-[23px] overflow-hidden flex items-center justify-start p-[20px] sm:p-[80px] bg-cover bg-center"
+                style={{ backgroundImage: 'url("/images/gallery/5.jpg")' }}
+            >
+                {/* Background overlay */}
+                <div className="absolute inset-0 bg-black/30 z-0"></div>
 
-                    </div>
+                {/* Content */}
+                <div className="relative z-10 w-[600px] p-8 pb-12 text-left">
+                    <h2 className="text-white text-[40px] mb-4 font-semibold leading-[1.2em]">We let our customers <br />
+                        talk for us.</h2>
+                    {/* Input Field and Subscribe Button */}
                 </div>
             </div>
+
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-1 place-items-center my-12 mx-8">
                 {testimonials.map(testimonial => (
                     <div key={testimonial.Id} className="h-[100%] mb-8">
