b6e325671681139dc0c08ebea45f1496026439d6
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-01-07 15:38:51 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import logo from "../public/images/logo.png";
 
 const NavBar: React.FC = () => {
-    const [isVisible, setIsVisible] = useState(true);
+    const [displayStyle, setDisplayStyle] = useState<"block" | "none">("block");
     const [activePage, setActivePage] = useState("");
     const [isMenuOpen, setIsMenuOpen] = useState(false);
     const [lastScrollTop, setLastScrollTop] = useState(0);
@@ -17,10 +17,10 @@ const NavBar: React.FC = () => {
     const handleScroll = () => {
         const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-        if (scrollTop > lastScrollTop && scrollTop > 20) {
-            setIsVisible(false);
+        if (scrollTop > lastScrollTop && scrollTop > 130) {
+            setDisplayStyle("none");
         } else {
-            setIsVisible(true);
+            setDisplayStyle("block");
         }
 
         setLastScrollTop(scrollTop <= 0 ? 0 : scrollTop);
@@ -43,60 +43,71 @@ const NavBar: React.FC = () => {
         { href: "/gallery", label: "Gallery" },
     ];
 
+    const handleLinkClick = (href: string) => {
+        setActivePage(href);
+        if (isMenuOpen) setIsMenuOpen(false); // Close the menu in mobile view when a link is clicked
+    };
+
     return (
         <div className="fixed top-0 left-0 w-full z-50">
             {/* Pre-header */}
-            <div
-                className={`bg-black text-white h-[57px] px-[65px] font-urbanist md:flex justify-center transition-all duration-300 hidden sm:block ${isVisible ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"}`}
-            >
-                <div className="flex justify-between items-center text-xs md:text-sm w-full max-w-screen-xl">
-                    {/* Contact Details */}
-                    <div className="flex gap-4">
-                        <span className="flex items-center gap-2">
-                            <span className="relative w-6 h-6 flex items-center justify-center rounded-full bg-white text-black">
-                                <i className="fas fa-phone fa-flip-horizontal text-xs" />
+            <div className="hidden sm:block">
+                <div
+                    className={`bg-black text-white h-[57px] py-[14px] px-[65px] font-urbanist md:flex justify-center transition-all duration-300`} style={{ display: displayStyle }}
+                >
+                    <div className="flex justify-between items-center text-xs md:text-sm w-full max-w-screen-xl">
+                        {/* Contact Details */}
+                        <div className="flex gap-4">
+                            <span className="flex items-center gap-2">
+                                <span className="relative w-6 h-6 flex items-center justify-center rounded-full bg-white text-black">
+                                    <i className="fas fa-phone fa-flip-horizontal text-xs" />
+                                </span>
+                                <a href="tel:+919873186168" className="text-white text-[16px]">+91 987 318 6168</a>
                             </span>
-                            <a href="tel:+919873186168" className="text-white text-[16px]">+91 987 318 6168</a>
-                        </span>
-                        <span className="flex items-center gap-2">
-                            <i className="fas fa-envelope text-[18px]" />
-                            <span className="flex flex-wrap">
-                                <a href="mailto:info@eazetours.com" className="text-white text-[16px]">info@eazetours.com</a>,
-                                <a href="mailto:harshit@eazetours.com" className="text-white text-[16px] ml-1">harshit@eazetours.com</a>
+                            <span className="flex items-center gap-2">
+                                <i className="fas fa-envelope text-[18px]" />
+                                <span className="flex flex-wrap">
+                                    <a href="mailto:info@eazetours.com" className="text-white text-[16px]">info@eazetours.com</a>,
+                                    <a href="mailto:harshit@eazetours.com" className="text-white text-[16px] ml-1">harshit@eazetours.com</a>
+                                </span>
                             </span>
-                        </span>
-                    </div>
-                    {/* Social Icons */}
-                    <div className="flex gap-2">
-                        <Link href="https://www.tripadvisor.in/Attraction_Review-g304551-d17734269-Reviews-EAZE_TOURS-New_Delhi_National_Capital_Territory_of_Delhi.html" passHref>
-                            <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300">
-                                <i className="fab fa-tripadvisor text-sm leading-lg" />
-                            </span>
-                        </Link>
-                        <Link href="https://www.instagram.com/eazetourpackages/" passHref>
-                            <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300">
-                                <i className="fab fa-instagram text-sm leading-lg" />
-                            </span>
-                        </Link>
-                        <Link href="https://www.facebook.com/eazetour/" passHref>
-                            <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300">
-                                <i className="fab fa-facebook text-sm leading-lg" />
-                            </span>
-                        </Link>
-                        <Link href="https://www.pinterest.com/eazetourpackages/" passHref>
-                            <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300">
-                                <i className="fab fa-pinterest text-sm leading-lg" />
-                            </span>
-                        </Link>
+                        </div>
+                        {/* Social Icons */}
+                        <div className="flex gap-2">
+                            <Link href="https://www.tripadvisor.in/Attraction_Review-g304551-d17734269-Reviews-EAZE_TOURS-New_Delhi_National_Capital_Territory_of_Delhi.html" passHref>
+                                <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300">
+                                    <i className="fab fa-tripadvisor text-sm leading-lg" />
+                                </span>
+                            </Link>
+                            <Link href="https://www.instagram.com/eazetourpackages/" passHref>
+                                <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300">
+                                    <i className="fab fa-instagram text-sm leading-lg" />
+                                </span>
+                            </Link>
+                            <Link href="https://www.facebook.com/eazetour/" passHref>
+                                <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300">
+                                    <i className="fab fa-facebook text-sm leading-lg" />
+                                </span>
+                            </Link>
+                            <Link href="https://www.pinterest.com/eazetourpackages/" passHref>
+                                <span className="relative w-7 h-7 flex items-center justify-center rounded-full bg-white text-black hover:bg-gray-300">
+                                    <i className="fab fa-pinterest text-sm leading-lg" />
+                                </span>
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
 
             {/* Navbar */}
             <div
-                className={`flex items-center w-full font-urbanist h-[78px] bg-white text-neutral transition-all duration-300 border-b border-gray-300 ${isVisible ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"}`}
+                className={`flex items-center w-full font-urbanist h-[78px] bg-white text-neutral transition-all duration-300 border-b border-gray-300`}
+                style={{ display: displayStyle }}
             >
-                <div className="navbar flex items-center justify-between" style={{ maxWidth: "1270px", margin: "0 auto" }}>
+                <div
+                    className="navbar flex py-[15px] items-center justify-between"
+                    style={{ maxWidth: "1270px", margin: "0 auto" }}
+                >
                     {/* Logo */}
                     <div className="navbar-start px-4 flex items-center" style={{ marginTop: "-10px" }}>
                         <Link href="/" passHref>
@@ -104,23 +115,24 @@ const NavBar: React.FC = () => {
                         </Link>
                     </div>
 
-                    {/* Hamburger Menu for Mobile */}
+                    {/* Hamburger Menu */}
                     <div className="md:hidden flex items-center pr-3 relative z-50">
                         <button onClick={() => setIsMenuOpen(!isMenuOpen)} className="text-[#025C7A]">
                             <i className={`fas ${isMenuOpen ? "fa-times" : "fa-bars"} text-xl`} />
                         </button>
                     </div>
 
-                    {/* Desktop Navigation Links (without Contact Us) */}
+                    {/* Desktop Navigation */}
                     <div className="navbar-center md:flex flex-grow hidden">
                         <div className="flex items-center justify-center gap-4">
                             {navLinks.map((link, index) => (
                                 <Link key={index} href={link.href} passHref>
                                     <button
+                                        onClick={() => handleLinkClick(link.href)}
                                         className={`btn btn-ghost btn-sm rounded-btn ${activePage === link.href
                                             ? "text-[#6E9753] text-[16px]"
-                                            : "text-[#025C7A]  text-[16px]"}`
-                                        }
+                                            : "text-[#025C7A] text-[16px]"
+                                            }`}
                                     >
                                         {link.label}
                                     </button>
@@ -129,27 +141,30 @@ const NavBar: React.FC = () => {
                         </div>
                     </div>
 
-                    {/* Mobile Sidebar Menu (with Contact Us) */}
+                    {/* Mobile Sidebar */}
                     <div
-                        className={`md:hidden fixed top-0 right-0 w-3/4 bg-white h-screen z-40 shadow-lg transform transition-transform duration-300 ${isMenuOpen ? "translate-x-0" : "translate-x-full"}`}
+                        className={`md:hidden fixed top-0 right-0 w-3/4 bg-white h-screen z-40 shadow-lg transform transition-transform duration-300 ${isMenuOpen ? "translate-x-0" : "translate-x-full"
+                            }`}
                     >
-                        <div className="flex flex-col text-left mr-auto -mt-[320px] items-start px-4 gap-4 py-16">
+                        <div className="flex flex-col text-left items-start px-4 gap-4 py-16">
                             {navLinks.map((link, index) => (
                                 <Link key={index} href={link.href} passHref>
                                     <button
+                                        onClick={() => handleLinkClick(link.href)}
                                         className={`btn btn-ghost btn-sm rounded-btn ${activePage === link.href
-                                            ? "text-[#6E9753]  text-[16px]"
-                                            : "text-[#025C7A]  text-[16px]"}`
-                                        }
+                                            ? "text-[#6E9753] text-[16px]"
+                                            : "text-[#025C7A] text-[16px]"
+                                            }`}
                                     >
                                         {link.label}
                                     </button>
                                 </Link>
                             ))}
+
                             {/* Add Contact Us only in Mobile Menu */}
                             <Link href="/contact" passHref>
                                 <button
-                                    className={`btn btn-ghost btn-sm rounded-btn ${activePage === "/contact" ? "text-[#6E9753]" : "text-[#025C7A]"}`
+                                    className={`btn btn-ghost btn-sm text-[16px] rounded-btn ${activePage === "/contact" ? "text-[#6E9753]" : "text-[#025C7A]"}`
                                     }
                                 >
                                     Contact Us
