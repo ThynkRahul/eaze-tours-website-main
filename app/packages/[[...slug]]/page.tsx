6c22c8c1ce6614f11816e5c49545d135e3fa6e79
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-01-16 12:28:30 +0530

    Create page.tsx

---
+++ app/packages/[[...slug]]/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import PackagesList from "../../../components/Packages";
+import PackageDetail from '../../../components/PackageDetail';
+import { notFound } from 'next/navigation';
+
+
+export default function Packages({ params }: { params: { slug?: string[] } }) {
+  const { slug } = params;
+  if (!slug) {
+    return (
+      <div>
+        <div className="mt-[78px] sm:mt-[135px]">
+          <PackagesList />
+        </div>
+      </div>
+    )
+  }
+  else if (slug.length == 1) {
+    const packageId = parseInt(slug[0])
+    if (packageId < 36 && packageId > 0 && !isNaN(packageId)) {
+      return (
+        <div>
+          <div className="mt-[78px] sm:mt-[135px]">
+            <PackageDetail packageId={packageId} />
+          </div>
+        </div>
+      )
+    }
+  }
+  return (
+    notFound()
+  )
+}
