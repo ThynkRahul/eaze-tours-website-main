9a8cf4e8e18c809cb7c534cbcd53c0fbe0519171
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-01-21 17:22:21 +0530

    Add files via upload

---
+++ app/blog/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import BlogList from "../../components/blogs";
+
+export default function BlogPage() {
+  return (
+    <div className="mt-16">
+      <BlogList />
+    </div>
+  );
+}
