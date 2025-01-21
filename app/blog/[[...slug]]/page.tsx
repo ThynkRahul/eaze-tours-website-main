581aa6cc288a87c0898c79a038993f35cf483689
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-01-21 17:40:02 +0530

    Update page.tsx

--- app/blog/[[...slug]]/page.tsx
+++ app/blog/[[...slug]]/page.tsx
@@ -1,22 +1,34 @@
 'use client';
 
-import BlogDetail from "../../../components/BlogDetail";
+import BlogList from "../../../components/blogs";
+import BlogDetail from '../../../components/BlogDetail';
 import { notFound } from 'next/navigation';
 
-interface BlogSlugProps {
-  params: { slug: string };
-}
 
-export default function BlogDetailPage({ params }: BlogSlugProps) {
-  const blogId = parseInt(params.slug);
-
-  if (blogId < 7 && blogId > 0 && !isNaN(blogId)) {
+export default function Blogs({ params }: { params: { slug?: string[] } }) {
+  const { slug } = params;
+  if (!slug) {
     return (
-      <div className="mt-16">
-        <BlogDetail blogId={blogId} />
+      <div>
+        <div className="mt-16">
+          <BlogList />
+        </div>
       </div>
-    );
+    )
   }
-
-  return notFound();
+  else if (slug.length == 1) {
+    const blogId = parseInt(slug[0])
+    if (blogId < 7 && blogId > 0 && !isNaN(blogId)) {
+      return (
+        <div>
+          <div className="mt-16">
+            <BlogDetail blogId={blogId} />
+          </div>
+        </div>
+      )
+    }
+  }
+  return (
+    notFound()
+  )
 }
