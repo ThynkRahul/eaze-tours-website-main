8c92665556fe270dff461679a49e3f23447cd2bb
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-02-03 13:36:35 +0530

    Update PackageDetail.tsx

--- components/PackageDetail.tsx
+++ components/PackageDetail.tsx
@@ -1,18 +1,20 @@
+'use client';
+
 import React from "react";
 import packageData from '../data/packages.json';
 import VerticalTimeline from './VerticalTimeline';
 
 interface IPackageDetailProp {
-    packageId?: number;
+    packageUri?: string;
 }
 
 
-export default function PackageDetail({ packageId }: IPackageDetailProp) {
-    const tourPackage = packageData.filter(function (tourPackage) { return tourPackage.Id == packageId; })[0];
+export default function PackageDetail({ packageUri }: IPackageDetailProp) {
+    const tourPackage = packageData.filter(function (tourPackage) { return tourPackage.Uri == packageUri; })[0];
 
     return (
         <>
-            <div className="mt-16 sm:mx-4">
+            <div className="mt-16 md:mx-16">
                 <VerticalTimeline tourPackage={tourPackage}>
                 </VerticalTimeline>
             </div>
