8cd95e83bf2f7f07b2220e92798f2c3c9244202d
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-02-14 16:42:15 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
       <div className="container mx-auto flex flex-col md:flex-row gap-0 justify-between p-6 md:p-10">
         {/* First Column - Logo, Content, and Social Icons */}
 
-        <div className="w-full md:w-1/4 mb-8 text-left pl-1 md:pl-8">
+        <div className="w-full md:w-[25%] mb-8 text-left pl-1 md:pl-8">
           <Link href="/" passHref><Image className="z-0 mb-4 md:mx-0" src={signature} width={200} height={87} alt="Logo"></Image></Link>
 
           <p className="text-sm mt-2 mb-4">Established in 2015, Eaze Tours is driven by three travel professionals, with decades of combined varied experience in the tour industry.</p>
@@ -30,22 +30,28 @@ function Footer() {
         </div>
 
         {/* Second Column - Useful Links */}
-        <div className="w-full md:w-1/4 mb-8 pl-1 md:pl-20 md:pt-10">
-
-          <div className="text-lg font-semibold mb-4">Useful Links</div>
-          <ul className="space-y-4">
-            <li><Link href="/about" className="hover:text-blue-600 transition-colors">About us</Link></li>
-            <li><Link href="/contact" className="hover:text-blue-600 transition-colors">Contact us</Link></li>
-            <li><Link href="/packages" className="hover:text-blue-600 transition-colors">Packages</Link></li>
-            <li><Link href="/gallery" className="hover:text-blue-600 transition-colors">Gallery</Link></li>
-            <li><Link href="/testimonials" className="hover:text-blue-600 transition-colors">Testimonials</Link></li>
+        <div className="w-full md:w-[38%] mb-8 pl-1 md:pl-10 md:pt-10">
+          <div className="text-lg font-semibold mb-4">India Best Tour Packages</div>
+          <ul className="grid grid-cols-2 gap-x-4 gap-y-2">
+            <li><Link href="/packages" className="hover:text-blue-600 transition-colors">Best Tours to India from USA</Link></li>
+            <li><Link href="/forts-and-palaces-india" className="hover:text-blue-600 transition-colors">Fort and Palaces of India</Link></li>
+            <li><Link href="/kerala-cochin-trivandrum-tour-india" className="hover:text-blue-600 transition-colors">Kerala Tour Package</Link></li>
+            <li><Link href="/royal-rajasthan-tour-package-india" className="hover:text-blue-600 transition-colors">Luxury Tour Package</Link></li>
+            <li><Link href="/wildlife-safari-tour-package-india" className="hover:text-blue-600 transition-colors">Wildlife Tour Package</Link></li>
+            <li><Link href="/tiger-safari-tour-india" className="hover:text-blue-600 transition-colors">Tiger Safari Tour Package</Link></li>
+            <li><Link href="/gujarat-tribal-tour-package-india" className="hover:text-blue-600 transition-colors">Tribal Tour Package India</Link></li>
+            <li><Link href="/south-india-tour-package-india" className="hover:text-blue-600 transition-colors">South India Tour Package</Link></li>
+            <li><Link href="/markha-trek-leh-travel-package" className="hover:text-blue-600 transition-colors">Trekking Tour Package in India</Link></li>
+            <li><Link href="/ayurveda-tour-package-kerala-india" className="hover:text-blue-600 transition-colors">Ayurveda Tour in India</Link></li>
+            <li><Link href="/romantic-andaman-tour-package-india" className="hover:text-blue-600 transition-colors">Honeymoon Tour Package India</Link></li>
+            <li><Link href="/chardham-pilgrimage-tour-package-india" className="hover:text-blue-600 transition-colors">Chardham Tour Package India</Link></li>
           </ul>
         </div>
 
         {/* Third Column - Top Destinations */}
-        <div className="w-full md:w-1/4 mb-8 pl-1 md:pl-20 md:pt-10">
+        <div className="w-full md:w-[17%] mb-8 pl-1 md:pl-12 md:pt-10">
           <div className="text-lg font-semibold mb-4">Top Destinations</div>
-          <ul className="space-y-4">
+          <ul className="space-y-2">
             <li><Link href="/packages/classical-delhi-jaipur-agra-khajurao-varanasi-tour-india" className="hover:text-blue-600 transition-colors">Delhi</Link></li>
             <li><Link href="/packages/forts-of-rajasthan-from-delhi-11-days-package-india" className="hover:text-blue-600 transition-colors">Jaipur</Link></li>
             <li><Link href="/packages/golden-triangle-delhi-agra-jaipur-7-days-package-india" className="hover:text-blue-600 transition-colors">Agra</Link></li>
@@ -55,7 +61,7 @@ function Footer() {
         </div>
 
         {/* Fourth Column - Contact Info */}
-        <div className="w-full md:w-1/4 mb-8 pl-1 md:pl-8 md:pt-10">
+        <div className="w-full md:w-[20%] mb-8 pl-1 md:pl-8 md:pt-10">
           <div className="text-lg font-semibold mb-4">Contact Us</div>
           <div className="space-y-4">
             <div className="flex items-start space-x-2">
