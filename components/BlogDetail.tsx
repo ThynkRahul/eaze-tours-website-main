2bc4764e0f7023e2bc1c6cc746a4835989b5b94f
ThynkNeeraj <thynk.neeraj@gmail.com>
2025-02-19 15:08:19 +0530

    Update BlogDetail.tsx

--- components/BlogDetail.tsx
+++ components/BlogDetail.tsx
@@ -16,13 +16,11 @@ export default function BlogDetail({ blogId }: BlogDetailProps) {
     return (
 
 
-        <div className="mt-[78px] sm:mt-[165px] mx-8 mb-12">
-
-
+        <div className="mt-[78px] sm:mt-[165px] mx-auto mb-12 max-w-[1200px] w-full px-4">
             <div className="py-6">
                 <h1 className="text-[30px] text-left font-semibold mb-8 sm:text-[45px]">{blog.Page_heading}</h1>
                 <div
-                    className="w-full h-[250px] sm:h-[600px] aspect-w-3 aspect-h-2 bg-cover rounded-[40px] bg-center shadow-[0px_0px_2px_1px_#00000040] mb-4 border border-gray-300"
+                    className="w-full h-[250px] sm:h-[600px] aspect-w-2 aspect-h-1 bg-cover rounded-[40px] bg-center shadow-[0px_0px_2px_1px_#00000040] mb-4 border border-gray-300"
                     style={{ backgroundImage: `url(${blog.Img})` }}
                 ></div>
                 <div dangerouslySetInnerHTML={{ __html: blog.Content }}
