ac4182ea41cd536f1293138d89d599635e02bf42
ThynkRahul <thynk.rahul@gmail.com>
2025-02-19 16:55:29 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = nextConfig
 module.exports = {
     async redirects() {
         return [
-            // Basic permanent redirect
+            
             {
                 source: '/packages/wildlife-tours',
                 destination: '/packages/wildlife-safari-tour-package-india',
